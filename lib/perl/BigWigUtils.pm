93da594cb421886f92a34da7c14807db503d79ef
steve-fischer-200 <sfischer@upenn.edu>
2023-05-29 21:58:03 -0400

    debug

--- lib/perl/BigWigUtils.pm
+++ lib/perl/BigWigUtils.pm
@@ -10,7 +10,9 @@ sub installBwFile {
   my ($userDatasetId, $bwFile) = @_;
 
   # initialize dir first time through
-  mkdir("$ENV{USER_DATASETS_DIR}/$userDatasetId") || die "Can't create user dataset directory '$ENV{USER_DATASETS_DIR}/$userDatasetId' $!" unless -d "$ENV{USER_DATASETS_DIR}/$userDatasetId";
+  unless ( -e "$ENV{USER_DATASETS_DIR}/$userDatasetId") {
+    mkdir("$ENV{USER_DATASETS_DIR}/$userDatasetId") || die "Can't create user dataset directory '$ENV{USER_DATASETS_DIR}/$userDatasetId' $!";
+  }
 
   copy($bwFile, "$ENV{USER_DATASETS_DIR}/$userDatasetId") or die "Copy of '$bwFile' to '$ENV{USER_DATASETS_DIR}/$userDatasetId' failed: $!";
 }
