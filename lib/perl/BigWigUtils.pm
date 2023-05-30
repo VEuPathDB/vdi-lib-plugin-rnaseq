6bec0d1d3944c17bea873d389c158dd2cc31be0c
steve-fischer-200 <sfischer@upenn.edu>
2023-05-30 07:46:17 -0400

    debug

--- lib/perl/BigWigUtils.pm
+++ lib/perl/BigWigUtils.pm
@@ -11,6 +11,7 @@ sub installBwFile {
 
   # initialize dir first time through
   unless ( -e "$ENV{USER_DATASETS_DIR}/$userDatasetId") {
+    print STDERR "Creating directory $ENV{USER_DATASETS_DIR}/$userDatasetId\n";
     mkdir("$ENV{USER_DATASETS_DIR}/$userDatasetId") || die "Can't create user dataset directory '$ENV{USER_DATASETS_DIR}/$userDatasetId' $!";
   }
 
@@ -25,8 +26,10 @@ sub deleteUserDatasetDir {
     foreach my $file (readdir $udDir) {
       next if $file =~ /^\.+$/;
       die "Unexpected directory '$file' in user dataset directory '$udDirPath'\n" if -d "$udDirPath/$file";
+      print STDERR "Deleting file $udDirPath/$file\n";
       unlink("$udDirPath/$file") || die "Could not delete file '$udDirPath/$file\n";
     }
+    print STDERR "Removing dir $udDirPath\n";
     rmdir($udDirPath) || die "Could not delete directory '$udDirPath'\n";
   }
 
