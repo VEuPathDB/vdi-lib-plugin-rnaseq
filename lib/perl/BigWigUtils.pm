896b691dc64bb5841fac808deba02141862c1831
steve-fischer-200 <sfischer@upenn.edu>
2024-02-07 13:29:49 -0500

    remove code that deletes DATA_FILES

--- lib/perl/BigWigUtils.pm
+++ lib/perl/BigWigUtils.pm
@@ -5,7 +5,7 @@ use Exporter;
 use File::Copy;
 use File::Basename;
 our @ISA = 'Exporter';
-our @EXPORT = qw(installBwFile deleteUserDatasetDir);
+our @EXPORT = qw(installBwFile);
 
 sub installBwFile {
   my ($bwFile, $dataFilesDir) = @_;
@@ -17,21 +17,5 @@ sub installBwFile {
   chmod(0664, "$dataFilesDir/$f") or die "Could not chmod $dataFilesDir/$f\n";
 }
 
-sub deleteUserDatasetDir {
-  my ($udDirPath) = @_;
-
-  if (-e $udDirPath) {
-    opendir(my $udDir, $udDirPath) or die "Cannot open directory '$udDirPath': $!\n";
-    foreach my $file (readdir $udDir) {
-      next if $file =~ /^\.+$/;
-      die "Unexpected directory '$file' in user dataset directory '$udDirPath'\n" if -d "$udDirPath/$file";
-      print STDERR "Deleting file $udDirPath/$file\n";
-      unlink("$udDirPath/$file") || die "Could not delete file '$udDirPath/$file\n";
-    }
-    print STDERR "Removing dir $udDirPath\n";
-    rmdir($udDirPath) || die "Could not delete directory '$udDirPath'\n";
-  }
-
-}
 
 1;
